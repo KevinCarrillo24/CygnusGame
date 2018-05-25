cefaca5d1655e64fcda94b5c368bc547cebc9412
Kevin Carrillo <kevin.carrillo@uao.edu.co>
2018-05-25 16:07:20 -0500

    Se arregla errores de AI del boss

--- Assets/_Scripts/ManeuverBoss.cs
+++ Assets/_Scripts/ManeuverBoss.cs
@@ -11,8 +11,10 @@ public class ManeuverBoss : MonoBehaviour {
     public Vector2 maneuverTime;
     public Vector2 maneuverWait;
     public Boundary boundary;
-    private bool wachito;
+    private bool isCreated;
+    private bool takle;
     public Vector3 finalPosition;
+    private float takleCount;
 
     private float targetManeuver;
     private float currentSpeed;
@@ -23,13 +25,15 @@ public class ManeuverBoss : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         currentSpeed = 0;
         StartCoroutine(Evade());
-        wachito = false;
+        isCreated = false;
+        takle = false;
+        takleCount = 0;
     }
     IEnumerator Evade()
     {
         yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
 
-        while (DestroyBoss.instace.life > 10)
+        while (DestroyBoss.instace.life > 1)
         {
             targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
             yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
@@ -39,7 +43,7 @@ public class ManeuverBoss : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        if (wachito)
+        if (isCreated)
         {
             float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smothing);
             rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
@@ -49,15 +53,33 @@ public class ManeuverBoss : MonoBehaviour {
                     0.0f,
                     Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
                 );
-            rb.rotation = Quaternion.Euler(0.0f, 180, rb.velocity.x * -tilt);
+            rb.rotation = Quaternion.Euler(0.0f, 180, 0.0f);
+            takleCount += Time.deltaTime;
+            if (takleCount > 5f)
+            {
+                takle = true;
+            }
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, finalPosition, Time.deltaTime * 3.0f);
-            if(transform.position == finalPosition)
+            if (transform.position == finalPosition)
             {
-                wachito = true;
+                isCreated = true;
+                takleCount = 0;
             }
         }
+        if (DestroyBoss.instace.life < 120 && isCreated && takle)
+        {
+            //rb.velocity = transform.forward * 5;
+            rb.velocity = new Vector3(0.0f, 0.0f, rb.velocity.z);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, 0f, 2f), Time.deltaTime * 7.0f);
+            if (transform.position.z < 2.1f)
+            {
+                isCreated = false;
+                takle = false;
+            }
+        }
+
     }
 }
